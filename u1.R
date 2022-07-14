885d5c0a680cb2c50f224bf57c8329a612ea36e7
Y Liu <yeqranzj@knowledgemd.com>
2022-07-14 13:44:56 -0400

    New visualizations

--- u1.R
+++ u1.R
@@ -4,7 +4,17 @@ library(ggplot2)
 library(dplyr)
 
 
-ggplot(titanic_data, aes(Age, Survived)) + 
-  geom_line() + 
+ggplot(titanic_data %>% filter(Survived == 1 & !is.na(Age)), 
+       aes(x = Age, color = Age)) + 
+  geom_histogram(position="identity", fill="white") + 
   labs(y="Count", title="Survival grouped by Age on the Titanic")
+
+
+
+
+
+survival_by_class <- titanic_data %>% filter(Age>20) %>% mutate(Survival = ifelse(Survived==1,"survived", "did not survive")) %>% group_by(Pclass, Survival) %>% summarise(count = n())
+ggplot(survival_by_class, aes(fill=as.factor(Pclass), y=count, x=Survival)) + 
+  geom_bar(position="stack",stat="identity") + 
+  labs(title="Survival by class on the Titanic in people older than 20")
 
